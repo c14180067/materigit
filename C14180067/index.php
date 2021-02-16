e6cda5a24d6c4d355ed015595fa3a2ca00255fc5
unknown <c14180067@john.petra.ac.id>
2021-02-16 19:42:54 +0700

    Poin D

--- C14180067/index.php
+++ C14180067/index.php
@@ -4,5 +4,18 @@
     </head>
     <body>
         <h2>C14180067 - Alfonsus Enrico</h2>
+        Select any date
+    <form method="post" action="">
+        <input type="date" name="date"/>
+        <input type="submit" name="submit" value="submit"/>
+    </form>
+    <?php
+        if(isset($_POST['submit'])) {
+            $now = date("Y-m-d");
+            $selected = $_POST['date'];
+            $value = ((abs(strtotime($now)-strtotime($selected)))/(60*60*24));
+            echo $value." days";
+        }
+    ?>
     </body>
 </html>
